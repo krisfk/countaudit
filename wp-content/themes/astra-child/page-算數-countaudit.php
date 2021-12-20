33a9371cefda7a52cb2a001122c886409478e5ab
kaiyu li <krisfk@gmail.com>
2021-12-21 04:03:54 +0800

    .

--- wp-content/themes/astra-child/page-算數-countaudit.php
+++ wp-content/themes/astra-child/page-算數-countaudit.php
@@ -173,7 +173,7 @@ get_header(); ?>
 
                 <div class="price-table">
 
-                    <div class="price-title white blue-bg">
+                    <div class="price-title white blue-bg text-center">
                         <h3 class="white">特快銀行開戶</h3>
                         <div>低至HK$1,000</div>
                     </div>
