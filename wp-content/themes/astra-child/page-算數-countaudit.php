308f935c1fe6f4ecddc4a61a1c92a5761b297897
kaiyu li <krisfk@gmail.com>
2021-12-20 19:08:58 +0800

    .

--- wp-content/themes/astra-child/page-算數-countaudit.php
+++ wp-content/themes/astra-child/page-算數-countaudit.php
@@ -10,7 +10,7 @@ get_header(); ?>
 <div class="main-banner">
 
     <img class="main-banner-img" src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/banner-bg.jpg" alt="">
-    <img src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/banner-bg.jpg" alt="">
+    <!-- <img src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/banner-bg.jpg" alt=""> -->
 
 </div>
 
