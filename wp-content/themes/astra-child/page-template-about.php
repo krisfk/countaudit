a3ed19b0468574c8fb474ea1a538fdaf17238d88
kaiyu li <krisfk@gmail.com>
2021-12-22 05:12:09 +0800

    .

--- wp-content/themes/astra-child/page-template-about.php
+++ wp-content/themes/astra-child/page-template-about.php
@@ -43,7 +43,7 @@ get_header(); ?>
     </div>
 
 
-    <div class="row justify-content-center">
+    <div class="row justify-content-center mt-5">
 
         <div class="col-3 mt-5">
             <div class=" grey-bg client-comment-div pe-4 ps-4 pt-5  pb-5  h-100">
