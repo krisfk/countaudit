cb75051e593a8fb3f165bcb42a6010490ffe8eb1
kaiyu li <krisfk@gmail.com>
2021-12-21 17:04:15 +0800

    .

--- wp-content/themes/astra-child/page-算數-countaudit.php
+++ wp-content/themes/astra-child/page-算數-countaudit.php
@@ -528,7 +528,13 @@ get_header(); ?>
         <div class="article-carousel">
 
             <div class="article-container"><img class="w-100"
-                    src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/dummy-article-img.png" alt=""></div>
+                    src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/dummy-article-img.png" alt="">
+
+                <div class="white-bg">
+                    <h3>僱員僱主文章</h3>
+                    <div>我們公司行政人手不足，導致帳目非常混亂。選用了CountAudit的雲端會計服務後，帳目立時變得清晰及容易查看！</div>
+                </div>
+            </div>
 
             <div class="article-container"><img class="w-100"
                     src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/dummy-article-img.png" alt=""></div>
