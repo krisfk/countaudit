0c340439ffc390256e62b3a6b9ad1e9df427d556
kaiyu li <krisfk@gmail.com>
2021-12-22 04:48:06 +0800

    .

--- wp-content/themes/astra-child/page-template-about.php
+++ wp-content/themes/astra-child/page-template-about.php
@@ -22,7 +22,9 @@ get_header(); ?>
 <div class="container banner-bottom-blue-container">
 
     <div class="p-4">
-        <h3 class="white">“令中小型規模公司的創業家們可以將所有煩人的行政財務工作交給我們，專心於發展自己的事業”</h3>
+        <h3 class="white text-center">“令中小型規模公司的創業家們可以將所有煩人的行政財務工作交給我們，專心於發展自己的事業”</h3>
+
+        <div class="text-end gold small-2">CountAudit宗旨</div>
     </div>
 </div>
 
