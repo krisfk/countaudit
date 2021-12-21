0e43fddd99ae4f019431291602d5d40b62c2af26
kaiyu li <krisfk@gmail.com>
2021-12-22 05:10:25 +0800

    .

--- wp-content/themes/astra-child/page-template-about.php
+++ wp-content/themes/astra-child/page-template-about.php
@@ -57,7 +57,7 @@ get_header(); ?>
 
 
                 </div>
-                <ul class="member-sm-ul p-0 mt-4 ms-0 me-0">
+                <ul class="member-sm-ul p-0 mt-3 ms-0 me-0">
                     <li><a href="#"><img src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/sm-icon-1.png"
                                 alt=""></a></li>
                     <li><a href="#"><img src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/sm-icon-2.png"
