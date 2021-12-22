b6a2431542efbd444868681c55e6859d7175391b
kaiyu li <krisfk@gmail.com>
2021-12-23 04:11:12 +0800

    .

--- wp-content/themes/astra-child/page-template-about.php
+++ wp-content/themes/astra-child/page-template-about.php
@@ -68,8 +68,6 @@ get_header(); ?>
                 <div class="respon blue-bg white">CPA (Practising)</div>
             </div>
 
-            <div class="member-title">PARTNER (SECRETARY)</div>
-
         </div>
         <div class="col-3 mt-5">
             <div class=" grey-bg client-comment-div pe-4 ps-4 pt-5  pb-5  h-100">
@@ -91,11 +89,9 @@ get_header(); ?>
                     <li><a href="#"><img src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/sm-icon-3.png"
                                 alt=""></a></li>
                 </ul>
+                <div class="respon blue-bg white">CPA (Practising)</div>
 
             </div>
-            <div class="member-title">PARTNER (SECRETARY)</div>
-
-
         </div>
         <div class="col-3 mt-5">
             <div class=" grey-bg client-comment-div pe-4 ps-4 pt-5  pb-5  h-100">
@@ -117,10 +113,9 @@ get_header(); ?>
                     <li><a href="#"><img src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/sm-icon-3.png"
                                 alt=""></a></li>
                 </ul>
+                <div class="respon blue-bg white">CPA (Practising)</div>
 
             </div>
-            <div class="member-title">insurance, MPF and banking</div>
-
         </div>
         <!-- <div class="col-4 mt-5">
             <div class=" grey-bg client-comment-div pe-4 ps-4 pt-5  pb-5 h-100">
