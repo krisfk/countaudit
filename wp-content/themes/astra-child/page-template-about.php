21656e750727c8a4a514d6d1259679f0feedee26
kaiyu li <krisfk@gmail.com>
2021-12-23 04:17:54 +0800

    .

--- wp-content/themes/astra-child/page-template-about.php
+++ wp-content/themes/astra-child/page-template-about.php
@@ -89,7 +89,7 @@ get_header(); ?>
                     <li><a href="#"><img src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/sm-icon-3.png"
                                 alt=""></a></li>
                 </ul>
-                <div class="respon blue-bg white">
+                <div class="respon  white">
                     <div class="skew-blue-bg"></div>
 
                     <span class="txt">CPA (Practising)</span>
