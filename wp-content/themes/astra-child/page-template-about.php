2bc6348b9d4fbaeb1ba564f4966951b22e508f31
kaiyu li <krisfk@gmail.com>
2021-12-23 04:18:54 +0800

    .

--- wp-content/themes/astra-child/page-template-about.php
+++ wp-content/themes/astra-child/page-template-about.php
@@ -65,7 +65,12 @@ get_header(); ?>
                                 alt=""></a></li>
                 </ul>
 
-                <div class="respon blue-bg white">CPA (Practising)</div>
+                <div class="respon  white">
+                    <div class="skew-blue-bg"></div>
+
+                    <span class="txt">CPA (Practising)</span>
+
+                </div>
             </div>
 
         </div>
@@ -118,8 +123,12 @@ get_header(); ?>
                     <li><a href="#"><img src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/sm-icon-3.png"
                                 alt=""></a></li>
                 </ul>
-                <div class="respon blue-bg white">insurance, MPF and banking</div>
+                <div class="respon  white">
+                    <div class="skew-blue-bg"></div>
 
+                    <span class="txt">insurance, MPF and banking</span>
+
+                </div>
             </div>
         </div>
         <!-- <div class="col-4 mt-5">
