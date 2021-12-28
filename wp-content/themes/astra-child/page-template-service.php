d803c0224759f7782ffcd32635f35100158b664c
kaiyu li <krisfk@gmail.com>
2021-12-28 17:28:11 +0800

    .

---
+++ wp-content/themes/astra-child/page-template-service.php
@@ -0,0 +1,389 @@
+<?php
+/**
+ * Template Name: Service page
+ * Template Post Type: page
+ **/
+
+
+// if ( ! defined( 'ABSPATH' ) ) {
+	// exit; // Exit if accessed directly.
+// }
+
+get_header(); ?>
+
+<div class="main-banner">
+
+    <img class="main-banner-img" src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/banner-bg.jpg" alt="">
+    <!-- <img src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/banner-bg.jpg" alt=""> -->
+
+</div>
+
+
+<div class="container banner-bottom-blue-container">
+
+    <div class="p-4">
+        <h3 class="white text-center">“令中小型規模公司的創業家們可以將所有煩人的行政財務工作交給我們，專心於發展自己的事業”</h3>
+
+        <div class="text-end gold small">CountAudit宗旨</div>
+    </div>
+</div>
+
+<div class="container mt-5">
+
+
+    <div class="text-center">
+        <div>
+            <h4 class="gold"> //Future-Oriented
+            </h4>
+        </div>
+        <div>
+            <h2 class="blue"> 我們的團隊</h2>
+            <div class="small-2">真誠服務社會，希望為商業社會帶來一點不一樣</div>
+        </div>
+    </div>
+
+
+    <div class="row justify-content-center mt-5">
+
+        <div class="col-3 mt-5">
+            <div class=" grey-bg client-comment-div pe-4 ps-4 pt-5  pb-5  h-100">
+                <img class="team-member-icon"
+                    src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/team-member-1.png" alt="">
+
+                <div class="blue mt-5 ps-3 pe-3">
+                    <h4 class="member-name blue m-0 ps-0 pe-0 pb-0 pt-2">SAM NG
+                    </h4>
+
+
+                </div>
+                <ul class="member-sm-ul p-0 mt-3 ms-0 me-0">
+                    <li><a href="#"><img src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/sm-icon-1.png"
+                                alt=""></a></li>
+                    <li><a href="#"><img src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/sm-icon-2.png"
+                                alt=""></a></li>
+                    <li><a href="#"><img src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/sm-icon-3.png"
+                                alt=""></a></li>
+                </ul>
+
+                <div class="respon  white">
+                    <div class="skew-blue-bg"></div>
+
+                    <span class="txt">CPA (Practising)</span>
+
+                </div>
+            </div>
+
+        </div>
+        <div class="col-3 mt-5">
+            <div class=" grey-bg client-comment-div pe-4 ps-4 pt-5  pb-5  h-100">
+                <img class="team-member-icon"
+                    src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/team-member-2.png" alt="">
+
+                <div class="blue mt-5 ps-3 pe-3">
+                    <h4 class="member-name blue m-0 ps-0 pe-0 pb-0 pt-2">DICKSON TSUI
+                    </h4>
+                    <div class="member-title">PARTNER (AUDIT)</div>
+
+
+                </div>
+                <ul class="member-sm-ul p-0 mt-3 ms-0 me-0">
+                    <li><a href="#"><img src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/sm-icon-1.png"
+                                alt=""></a></li>
+                    <li><a href="#"><img src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/sm-icon-2.png"
+                                alt=""></a></li>
+                    <li><a href="#"><img src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/sm-icon-3.png"
+                                alt=""></a></li>
+                </ul>
+                <div class="respon  white">
+                    <div class="skew-blue-bg"></div>
+
+                    <span class="txt">CPA (Practising)</span>
+
+                </div>
+
+            </div>
+        </div>
+        <div class="col-3 mt-5">
+            <div class=" grey-bg client-comment-div pe-4 ps-4 pt-5  pb-5  h-100">
+                <img class="team-member-icon"
+                    src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/team-member-3.png" alt="">
+
+                <div class="blue mt-5 ps-3 pe-3">
+                    <h4 class="member-name blue m-0 ps-0 pe-0 pb-0 pt-2">ALEX CHEUNG
+                    </h4>
+                    <div class="member-title">PARTNER (FINANCE)</div>
+
+
+                </div>
+                <ul class="member-sm-ul p-0 mt-3 ms-0 me-0">
+                    <li><a href="#"><img src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/sm-icon-1.png"
+                                alt=""></a></li>
+                    <li><a href="#"><img src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/sm-icon-2.png"
+                                alt=""></a></li>
+                    <li><a href="#"><img src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/sm-icon-3.png"
+                                alt=""></a></li>
+                </ul>
+                <div class="respon  white">
+                    <div class="skew-blue-bg"></div>
+
+                    <span class="txt">insurance, MPF and banking</span>
+
+                </div>
+            </div>
+        </div>
+        <!-- <div class="col-4 mt-5">
+            <div class=" grey-bg client-comment-div pe-4 ps-4 pt-5  pb-5 h-100">
+
+                <img class="team-member-icon"
+                    src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/team-member-2.png" alt="">
+
+                <div class="text-center">
+                    <h4 class="blue m-0 ps-0 pe-0 pb-0 pt-2">IDRPHK LIMITED</h4>
+                    <div class="small gold">香港綜合債務重組機構</div>
+                </div>
+
+                <div class="mt-4">
+                    感謝CountAudit 在公司的出入帳上提供莫大的幫助。基於會計師的專業，令凌亂的帳目得以改善。CountAudit 將會在日後與我們機構維持良好的長期合作關係。</div>
+            </div>
+
+        </div>
+        <div class="col-4 mt-5">
+            <div class=" grey-bg client-comment-div pe-4 ps-4 pt-5  pb-5  h-100">
+
+                <img class="team-member-icon"
+                    src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/team-member-3.png" alt="">
+
+                <div class="text-center">
+                    <h4 class="blue m-0 ps-0 pe-0 pb-0 pt-2">Dream Impact 夢創成真</h4>
+                    <div class="small gold">社創工作空間</div>
+                </div>
+
+                <div class="mt-4">
+                    Sam 是一個貼心的服務提供者。由了解我嘅需要到設計貼身嘅會計服務，整個過程都好順暢，令我可以專注於業務。</div>
+            </div>
+        </div> -->
+
+    </div>
+
+
+
+
+</div>
+
+
+<div class="blue-bg mt-5">
+
+    <div class="container pt-5 pb-5">
+
+        <div class="about-carousel">
+
+
+            <div>
+                <div class="row justify-content-center gx-0 align-items-middle">
+
+                    <div class="col-5">
+
+                        <img src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/about-carousel-img-1.png"
+                            alt="">
+                    </div>
+                    <div class="col-5 right-content-div white-bg position-relative">
+
+                        <div class="text-center inner-div">
+
+
+                            <img class="about-carousel-icon"
+                                src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/about-carousel-img-1-icon.png"
+                                alt="">
+                            <h4 class="gold mt-3"> 加快資訊傳遞</h4>
+
+                            <div class="blue mt-3">不斷找出最新商業財務準則之更新，執法部門之行動，業內最新消息，令重要資訊更快廣為人知。</div>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div>
+
+                <div class="row justify-content-center gx-0 align-items-middle">
+
+                    <div class="col-5">
+
+                        <img src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/about-carousel-img-2.png"
+                            alt="">
+                    </div>
+                    <div class="col-5 right-content-div white-bg position-relative">
+
+                        <div class="text-center inner-div">
+
+
+                            <img class="about-carousel-icon"
+                                src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/about-carousel-img-2-icon.png"
+                                alt="">
+                            <h4 class="gold mt-3"> 增強小機構業務力量</h4>
+
+                            <div class="blue mt-3">
+                                協助小型機構，尤其是不太擅長市場推廣的NGO非弁利機構、社會企業和初創小型公司。提供市場商業建議，協助他們實行更有效率管理，省下時間專注服務社會。</div>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div>
+
+                <div class="row justify-content-center gx-0 align-items-middle">
+
+                    <div class="col-5">
+
+                        <img src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/about-carousel-img-3.png"
+                            alt="">
+                    </div>
+                    <div class="col-5 right-content-div white-bg position-relative">
+
+                        <div class="text-center inner-div">
+
+
+                            <img class="about-carousel-icon"
+                                src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/about-carousel-img-3-icon.png"
+                                alt="">
+                            <h4 class="gold mt-3"> 普及商業知識</h4>
+
+                            <div class="blue mt-3">使香港各界人士對商業及財務知識更有了解，為自己爭取應得之福祉，例如減少僱員/自僱人士被僱主剝削。</div>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div>
+
+                <div class="row justify-content-center gx-0 align-items-middle">
+
+                    <div class="col-5">
+
+                        <img src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/about-carousel-img-4.png"
+                            alt="">
+                    </div>
+                    <div class="col-5 right-content-div white-bg position-relative">
+
+                        <div class="text-center inner-div">
+
+
+                            <img class="about-carousel-icon"
+                                src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/about-carousel-img-4-icon.png"
+                                alt="">
+                            <h4 class="gold mt-3"> 連結社會創新企業</h4>
+
+                            <div class="blue mt-3">締結社會創新圈子，令創業家能夠相互幫助，達至社會共贏</div>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</div>
+
+
+<div class="grey-bg-2 mt-5">
+
+
+    <div class="container">
+        <div class="row  mt-5 align-items-center">
+            <div class="col-6">
+
+                <div>
+                    <h2 class="blue">聯絡我們</h2>
+
+                    <table class="border-0 contact-table">
+                        <tr>
+                            <td><img src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/contact-icon-1-1.png"
+                                    alt="">
+
+                            </td>
+                            <td>
+                                <div>Lai Chi Kok Office <br>
+                                    Flat C, 11/F., Wing Hong Centre, 18 Wing Hong Street, Lai Chi Kok,
+                                    Kowloon<br>
+                                    Unit C, 4/F, 760 Cheung Sha Wan Rd, Lai Chi Kok, Kowloon<br>
+                                    Kwun Tong Office<br>
+                                    Unit 9039, 9/F, Block B, Chung Mei Centre, 15-17 Hing Yip Street,
+                                    Kwun
+                                    Tong,<br>
+                                    Kowloon, Hong Kong</div>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td><img src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/contact-icon-2-1.png"
+                                    alt=""> </td>
+                            <td>3525 1900 <br>
+                                Office hour: Mon to Fri 10am - 7pm<br>
+                                6519 4755 (Whatsapp Only)</td>
+                        </tr>
+                        <tr>
+                            <td><img src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/contact-icon-3-1.png"
+                                    alt=""></td>
+                            <td>
+                                <a href="mailto:info@countaudit.hk" target="_blank">info@countaudit.hk</a>
+
+                                <!--  -->
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+            <div class="col-6 p-5">
+
+                <div class="form-div white-bg ">
+
+                    <input type="text" class="form-control" placeholder="如何稱呼您？">
+                    <input type="text" class="form-control" placeholder="聯絡電話">
+                    <input type="text" class="form-control" placeholder="電郵">
+                    <input type="text" class="form-control" placeholder="查詢類別">
+                    <textarea class="form-control" name="" id="" cols="30" rows="10" placeholder="簡短留言"></textarea>
+
+                    <div class="text-end">
+                        <a href="#" class="blue-bg white submit-btn mt-3">服務報價</a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<?php get_footer(); ?>
+<script type="text/javascript">
+$(function() {
+    $('.about-carousel').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
+        autoplay: false,
+        pauseOnFocus: false,
+        infinite: true,
+        speed: 800,
+        autoplaySpeed: 5000,
+        cssEase: 'ease-out',
+        pauseOnHover: false
+
+    });
+
+
+
+
+})
+</script>
+</body>
+
+</html>
