b1120fc59e5f43327dc5a296ae4fe9c16c14f5d1
kaiyu li <krisfk@gmail.com>
2021-12-29 03:49:21 +0800

    .

--- wp-content/themes/astra-child/page-template-service.php
+++ wp-content/themes/astra-child/page-template-service.php
@@ -19,6 +19,7 @@ get_header(); ?>
             <div><?php echo get_field('title_txt');?></div>
         </div>
     </div>
+
     <img class="main-banner-img" src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/banner-bg.jpg" alt="">
     <!-- <img src="http://64.227.13.14/countaudit/wp-content/uploads/2021/12/banner-bg.jpg" alt=""> -->
 
