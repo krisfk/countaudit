cd9089e3b81fd606490442cac9d48401cafe2cf8
kaiyu li <krisfk@gmail.com>
2021-12-30 04:42:30 +0800

    .

--- wp-content/themes/astra-child/page-article.php
+++ wp-content/themes/astra-child/page-article.php
@@ -39,7 +39,7 @@ get_header(); ?>
 
 <div id="primary" <?php astra_primary_class(); ?>>
 
-    <?php astra_primary_content_top(); ?>
+    <?php //astra_primary_content_top(); ?>
 
     <?php astra_content_page_loop(); ?>
 
