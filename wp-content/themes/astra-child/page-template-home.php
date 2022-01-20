8a10e9052f7355c1b16cd20c4bbc4b34f255659b
kaiyu li <krisfk@gmail.com>
2022-01-21 04:03:36 +0800

    .

--- wp-content/themes/astra-child/page-template-home.php
+++ wp-content/themes/astra-child/page-template-home.php
@@ -905,8 +905,8 @@ get_header(); ?>
 $(function() {
     $('.article-carousel').slick({
         infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
+        // slidesToShow: 3,
+        // slidesToScroll: 3,
         dots: true,
         arrows: false,
         autoplay: false,
@@ -917,14 +917,20 @@ $(function() {
         cssEase: 'ease-out',
         pauseOnHover: false,
         responsive: [{
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: true
+                breakpoint: 1920,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
             }
-        }]
+        ]
 
     });
 
