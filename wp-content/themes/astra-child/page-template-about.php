80cf9d153a1d13589af417a63dd0fd2745715ade
kaiyu li <krisfk@gmail.com>
2022-01-21 04:55:14 +0800

    .

--- wp-content/themes/astra-child/page-template-about.php
+++ wp-content/themes/astra-child/page-template-about.php
@@ -16,9 +16,9 @@ get_header(); ?>
     <!-- <div class="position-absolute top-0 start-0 w-100 h-100"> -->
 
     <!-- <div> -->
-    <!-- <div class="container title-container">
+    <div class="container title-container">
         <div><?php echo get_field('title_txt');?></div>
-    </div> -->
+    </div>
     <!-- </div> -->
 
 
