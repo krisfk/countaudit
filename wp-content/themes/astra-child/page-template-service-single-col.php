fb71cde0df23d67ef8bfb18e2c679f11b2beb343
kaiyu li <krisfk@gmail.com>
2022-01-21 17:29:49 +0800

    .

--- wp-content/themes/astra-child/page-template-service-single-col.php
+++ wp-content/themes/astra-child/page-template-service-single-col.php
@@ -301,9 +301,9 @@ get_header(); ?>
 <div class="grey-bg-2 mt-5">
 
 
-    <div class="container">
+    <div class="container contact-form-container">
         <div class="row pt-5 pb-5  mt-5 align-items-center justify-content-center">
-            <div class="col-5">
+            <div class="col-lg-6 col-md-12 col-sm-12 col-12  p-lg-5 p-md-5 p-sm-3 p-3 ">
 
                 <div>
                     <div>
