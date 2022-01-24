7b961beaf9fbfd0343b4e74eda0e3da24b4fe41c
kaiyu li <krisfk@gmail.com>
2022-01-24 18:21:05 +0800

    .

--- wp-content/themes/astra-child/page-template-service.php
+++ wp-content/themes/astra-child/page-template-service.php
@@ -409,7 +409,7 @@ get_header(); ?>
 
                         <div class="col-3 mb-3"></div>
 
-                        <div class="col-3"></div>
+                        <div class="col-3 mb-3"></div>
 
 
 
