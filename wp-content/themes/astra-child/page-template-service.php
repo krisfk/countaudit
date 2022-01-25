01845dca70e79104e916503afed38ed4b494e2c9
kaiyu li <krisfk@gmail.com>
2022-01-26 04:45:32 +0800

    .

--- wp-content/themes/astra-child/page-template-service.php
+++ wp-content/themes/astra-child/page-template-service.php
@@ -215,8 +215,20 @@ get_header(); ?>
                             <div class="col-12 mt-4">
                                 <h4 class="text-center mb-4"> 公司資料 Company Information</h4>
                             </div>
+                            <div class="col-12">公司名稱 Company Name＊"</div>
 
-
+                            <div class="col-lg-6 col-md-6 col-sm-12 col-12  mb-3"><label for="contact-name">
+                                    中文名稱 Chinese Name</label>
+                            </div>
+                            <div class="col-lg-6 col-md-6 col-sm-12 col-12  mb-3"><input id="contact-name"
+                                    name="contact-name" type="text" class="form-control">
+                            </div>
+                            <div class="col-lg-6 col-md-6 col-sm-12 col-12  mb-3"><label for="contact-name">
+                                    英文名稱 English Name</label>
+                            </div>
+                            <div class="col-lg-6 col-md-6 col-sm-12 col-12  mb-3"><input id="contact-name"
+                                    name="contact-name" type="text" class="form-control">
+                            </div>
 
 
 
