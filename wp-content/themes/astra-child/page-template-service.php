007659252fcbc9556cdef6cfebb7f345458774b8
kaiyu li <krisfk@gmail.com>
2022-01-26 05:13:05 +0800

    .

--- wp-content/themes/astra-child/page-template-service.php
+++ wp-content/themes/astra-child/page-template-service.php
@@ -273,10 +273,12 @@ get_header(); ?>
 
 
 
-                            <div class="col-lg-6 col-md-6 col-sm-12 col-12  mb-3"><label for="upload-file-2">變動詳情
+                            <div class="col-12 mt-4">
+                                <h4 class="text-center mb-4"> 變動詳情
 
-                                    Details of changes</label>
+                                    Details of changes</h4>
                             </div>
+
                             <div class="col-lg-6 col-md-6 col-sm-12 col-12  mb-3">
 
                                 <!-- <input type="file" class="form-control" name="upload-file-2" id="upload-file-2"> -->
