2ad00f65633259a106abc999cbb49f1632ed84d7
kaiyu li <krisfk@gmail.com>
2022-01-26 05:30:25 +0800

    .

--- wp-content/themes/astra-child/page-template-service.php
+++ wp-content/themes/astra-child/page-template-service.php
@@ -357,15 +357,17 @@ get_header(); ?>
                                     <input type="text" class="form-control">
                                 </div>
 
-                                <div class="col-lg-6 col-md-6 col-sm-12 col-12  mb-3"><label for="remarks">
-                                        備註 Remarks</label>
-                                </div>
-                                <div class="col-lg-6 col-md-6 col-sm-12 col-12  mb-3"><input id="remarks" name="remarks"
-                                        type="text" class="form-control">
-                                </div>
-
 
                             </div>
+
+
+                            <div class="col-lg-6 col-md-6 col-sm-12 col-12  mb-3"><label for="remarks">
+                                    備註 Remarks</label>
+                            </div>
+                            <div class="col-lg-6 col-md-6 col-sm-12 col-12  mb-3"><input id="remarks" name="remarks"
+                                    type="text" class="form-control">
+                            </div>
+
 
                         </div>
 
