b7e9598fda8cc2683497e354f257d54d1640c0ca
kaiyu li <krisfk@gmail.com>
2022-02-05 02:57:46 +0800

    .

--- wp-content/themes/astra-child/page-template-service-single-col.php
+++ wp-content/themes/astra-child/page-template-service-single-col.php
@@ -571,6 +571,28 @@ get_header(); ?>
 
 
                             </div>
+                            <div class="col-12 mt-4">
+                                <h4 class="text-center mb-4"> 服務計劃資料 Service Plan Information </h4>
+                                <div>
+                                    <b>
+                                        服務費用 (每年港幣$950) Service fee (per year HK$950)，已包括：</b>
+
+
+                                    <ul class="m-0 mt-2 p-0 ps-3">
+                                        <li>擔任法定公司秘書 Act as Company Secretary</li>
+                                        <li>準備及遞交週年申報表 Prepare and Submit Annual Return</li>
+                                        <li>協助續辦商業登記證 Renew the Business Registration Certificate</li>
+                                        <li> 對公司註冊署及稅局信提供專業意見 Provide professional opinions for CR and IRD’s letter</li>
+                                        <li>更改公司註冊地址資料 Change of Registered Office Address</li>
+                                        <li>準備及遞交任免董事資料 Appointment / Resignation / Change of Director</li>
+                                        <li>準備及遞交任免秘書資料 Appointment / Resignation / Change of Secretary</li>
+                                        <li>更改董事個人資料 Change of Personal Particulars of Director</li>
+                                        <li>業務性質變更 Change of Company Business Nature</li>
+                                        <li>準備及保存重要控制人登記册 Prepare and keep record of Significant Controllers Register
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
 
 
