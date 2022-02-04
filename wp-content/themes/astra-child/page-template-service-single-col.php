6ba119977fa0d21a5836ef794c52d9416a8560de
kaiyu li <krisfk@gmail.com>
2022-02-05 04:13:48 +0800

    .

--- wp-content/themes/astra-child/page-template-service-single-col.php
+++ wp-content/themes/astra-child/page-template-service-single-col.php
@@ -664,6 +664,7 @@ function generate_fill_area(no_of_applicant, applicant_fill_html) {
 }
 
 function add_fill_area(i) {
+    alert(applicant_fill_html);
     applicant_fill_html +=
         '<div class="row applicant-div mx-auto mb-3 "> <div class="col-lg-6 col-md-6 col-sm-12 col-12 mb-3"><label for="language">' +
         i +
