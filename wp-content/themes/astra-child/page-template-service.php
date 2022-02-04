808a5d7ecedec45dbcb6308df0c2f7929b5339f5
kaiyu li <krisfk@gmail.com>
2022-02-04 22:43:29 +0800

    .

--- wp-content/themes/astra-child/page-template-service.php
+++ wp-content/themes/astra-child/page-template-service.php
@@ -1676,7 +1676,7 @@ Existing Customers renewal to CountAudit Secretarial Limited with not less  than
         // var tnc = '<?php// echo get_field('tnc');?>';
         // $('#tnc').html('<?php echo get_field('tnc');?>');
         // $('#tnc').val(div.replace("\\n","\n"));
-
+        alert(90)
         $('#tnc').html($('#tnc-data').html());
         alert(80);
         $('.form-submit-btn').click(function() {
