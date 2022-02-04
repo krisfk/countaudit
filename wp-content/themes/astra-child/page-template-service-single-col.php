4bb5b6dd7f5a1176be03c9fe81313f589e8c8ae1
kaiyu li <krisfk@gmail.com>
2022-02-05 04:15:18 +0800

    .

--- wp-content/themes/astra-child/page-template-service-single-col.php
+++ wp-content/themes/astra-child/page-template-service-single-col.php
@@ -618,6 +618,9 @@ get_header(); ?>
 
 <?php get_footer(); ?>
 <script type="text/javascript">
+var no_of_applicant = 1;
+var applicant_fill_html = '';
+
 function generate_fill_area(no_of_applicant, applicant_fill_html) {
     for (i = 1; i <= no_of_applicant; i++) {
         applicant_fill_html +=
@@ -704,8 +707,7 @@ function add_fill_area(i) {
 
 $(function() {
 
-    var no_of_applicant = 1;
-    var applicant_fill_html = '';
+
 
 
     generate_fill_area(no_of_applicant, applicant_fill_html);
