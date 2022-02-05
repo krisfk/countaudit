880d24ffa2c22cc0c8798dc27c52d02d6e3ce140
kaiyu li <krisfk@gmail.com>
2022-02-05 16:09:29 +0800

    .

--- wp-content/themes/astra-child/page-template-service-single-col.php
+++ wp-content/themes/astra-child/page-template-service-single-col.php
@@ -591,7 +591,7 @@ get_header(); ?>
 
                                 <div class="row">
 
-
+                                    <!--  -->
                                     <div class="col-lg-6 col-md-6 col-sm-12 col-12  mb-3"><label
                                             for="email">上載股東及董事資料文件</label>
                                     </div>
