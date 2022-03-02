920a381cd7fa4bc9bb21d5cb31c467104aaf51ac
kaiyu li <krisfk@gmail.com>
2022-03-03 06:06:05 +0800

    .

--- wp-content/themes/astra-child/page-print.php
+++ wp-content/themes/astra-child/page-print.php
@@ -3,6 +3,8 @@ get_header();
 ?>
 
 <div class="container mx-auto mt-4">
+
+
     <div class="print-header">
 
         <img class="ca-print-logo" src="<?php echo get_site_url();?>/wp-content/uploads/2022/03/ca-print-logo-1.jpg"
@@ -12,150 +14,151 @@ get_header();
             信托或公司服務提供牌照:TC007204</div>
     </div>
 
-    <h2 class="text-center mt-4">Quotation Form of Accounting Service <br>
-        會計服務報價表
-    </h2>
+    <div class="d-none">
+        <h2 class="text-center mt-4">Quotation Form of Accounting Service <br>
+            會計服務報價表
+        </h2>
 
-    <table class="form-table mt-3 mb-0">
-        <tr>
-            <td colspan="2" class="text-center orange-yellow-bg fw-bold">Contact Information 聯絡資料</td>
-        </tr>
-        <tr>
-            <td class="fit">
-                Name 姓名:</td>
-            <td></td>
-        </tr>
-        <tr>
-            <td class="fit">
-                Tel 電話:</td>
-            <td></td>
-        </tr>
-        <tr>
-            <td class="fit">
-                E-mail 電郵:</td>
-            <td></td>
-        </tr>
-    </table>
-
-
-    <table class="form-table mt-0">
-        <tr>
-            <td colspan="4" class="text-center orange-yellow-bg fw-bold">Company Information 公司資料</td>
-        </tr>
-        <tr>
-            <td class="fit">Company name <br> 公司名稱</td>
-            <td colspan="3">
-            </td>
-        </tr>
-        <tr>
-            <td class="fit">
-                Principal activities <br>
-                主要業務
-            </td>
-            <td colspan="3">
-            </td>
-        </tr>
-        <tr>
-            <td class="fit">Incorporation date <br>
-                公司開立日期</td>
-            <td colspan="3"></td>
-        </tr>
-        <tr>
-            <td class="fit">Has performed audit before?<br>
-                是否曾進行核數</td>
-            <td class="fit">Yes / No If Yes,<br>please provide the related year ended date<br>
-                是 / 否 如是，請提供相關年結日</td>
-            <td class="text-wrap" colspan="2"></td>
-        </tr>
-        <tr>
-            <td class="fit">Has declared profits tax before?<br>
-                是否曾申報利得稅</td>
-            <td class="fit">Yes / No If Yes,<br>please provide the related year of assessment<br>
-                是 / 否 如是，請提供相關課稅年度</td>
-            <td class="text-wrap" colspan="2"></td>
-        </tr>
-        <tr>
-            <td class="fit">Has done accounting before?<br>
-                是否曾做會計帳</td>
-            <td class="fit">Yes / No If Yes,<br>please provide the related year ended date<br>
-                是 / 否 如是，請提供相關年結日</td>
-            <td class="text-wrap" colspan="2"></td>
-        </tr>
-        <tr>
-            <td class="fit">Reporting Frequency <br>
-                會計匯報頻率</td>
-            <td colspan="3">
-                每月 Monthly □ 每季 Quarterly □ 每年 Yearly
-            </td>
-        </tr>
-        <tr>
-            <td>Has done Excel for business records? <br>
-                有否做Excel營業記錄？ </td>
-            <td colspan="3">
-
-                Yes / No, if Yes, please upload <br>
-                　如有，請上載</td>
-        </tr>
-        <tr>
-            <td>Way of sorting and keeping receipts <br>
-                整理單據的方式<br>
-
-                <div class="small"> e.g. Scanning / taking picture /<br>sorted by folder / unsorted <br>
-                    例如電子掃描//拍照/<br>分類袋分類/沒有任何分類</div>
-            </td>
-            <td colspan="3">
-
-            </td>
-        </tr>
-
-        <tr>
-            <td>Total Turnover (yearly) <br>
-                全年總營業額</td>
-            <td colspan="3">
-
-            </td>
-        </tr>
-        <tr>
-            <td class="fit">Any stock? <br>
-                有否存貨?</td>
-            <td class="text-wrap">Yes/No</td>
-            <td class="fit">No. of bank used <br>
-                使用銀行的數量</td>
-            <td class="text-wrap"></td>
-        </tr>
-
-        <tr>
-            <td class="fit">No. of bank transactions <br> (monthly) <br>
-                每月經銀行交易的數量</td>
-            <td class="text-wrap"></td>
-            <td class="fit">No. of non-bank transactions <br>(monthly) <br>
-                每月非經銀行交易的數量</td>
-            <td class="text-wrap"></td>
-        </tr>
-
-        <tr>
-            <td class="fit">Any property? <br>
-                有否物業?</td>
-            <td class="text-wrap">Yes /No</td>
-            <td class="fit">Any motor vehicle? <br>
-                有否車輛?</td>
-            <td class="text-wrap">Yes /No</td>
-        </tr>
+        <table class="form-table mt-3 mb-0">
+            <tr>
+                <td colspan="2" class="text-center orange-yellow-bg fw-bold">Contact Information 聯絡資料</td>
+            </tr>
+            <tr>
+                <td class="fit">
+                    Name 姓名:</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td class="fit">
+                    Tel 電話:</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td class="fit">
+                    E-mail 電郵:</td>
+                <td></td>
+            </tr>
+        </table>
 
 
-        <tr>
-            <td class="fit">No. of loans / hire purchases <br>
-                貸款/分期付款數目</td>
-            <td class="text-wrap"></td>
-            <td class="fit">No. of employees <br>
-                員工人數</td>
-            <td class="text-wrap"></td>
-        </tr>
+        <table class="form-table mt-0">
+            <tr>
+                <td colspan="4" class="text-center orange-yellow-bg fw-bold">Company Information 公司資料</td>
+            </tr>
+            <tr>
+                <td class="fit">Company name <br> 公司名稱</td>
+                <td colspan="3">
+                </td>
+            </tr>
+            <tr>
+                <td class="fit">
+                    Principal activities <br>
+                    主要業務
+                </td>
+                <td colspan="3">
+                </td>
+            </tr>
+            <tr>
+                <td class="fit">Incorporation date <br>
+                    公司開立日期</td>
+                <td colspan="3"></td>
+            </tr>
+            <tr>
+                <td class="fit">Has performed audit before?<br>
+                    是否曾進行核數</td>
+                <td class="fit">Yes / No If Yes,<br>please provide the related year ended date<br>
+                    是 / 否 如是，請提供相關年結日</td>
+                <td class="text-wrap" colspan="2"></td>
+            </tr>
+            <tr>
+                <td class="fit">Has declared profits tax before?<br>
+                    是否曾申報利得稅</td>
+                <td class="fit">Yes / No If Yes,<br>please provide the related year of assessment<br>
+                    是 / 否 如是，請提供相關課稅年度</td>
+                <td class="text-wrap" colspan="2"></td>
+            </tr>
+            <tr>
+                <td class="fit">Has done accounting before?<br>
+                    是否曾做會計帳</td>
+                <td class="fit">Yes / No If Yes,<br>please provide the related year ended date<br>
+                    是 / 否 如是，請提供相關年結日</td>
+                <td class="text-wrap" colspan="2"></td>
+            </tr>
+            <tr>
+                <td class="fit">Reporting Frequency <br>
+                    會計匯報頻率</td>
+                <td colspan="3">
+                    每月 Monthly □ 每季 Quarterly □ 每年 Yearly
+                </td>
+            </tr>
+            <tr>
+                <td>Has done Excel for business records? <br>
+                    有否做Excel營業記錄？ </td>
+                <td colspan="3">
+
+                    Yes / No, if Yes, please upload <br>
+                    　如有，請上載</td>
+            </tr>
+            <tr>
+                <td>Way of sorting and keeping receipts <br>
+                    整理單據的方式<br>
+
+                    <div class="small"> e.g. Scanning / taking picture /<br>sorted by folder / unsorted <br>
+                        例如電子掃描//拍照/<br>分類袋分類/沒有任何分類</div>
+                </td>
+                <td colspan="3">
+
+                </td>
+            </tr>
+
+            <tr>
+                <td>Total Turnover (yearly) <br>
+                    全年總營業額</td>
+                <td colspan="3">
+
+                </td>
+            </tr>
+            <tr>
+                <td class="fit">Any stock? <br>
+                    有否存貨?</td>
+                <td class="text-wrap">Yes/No</td>
+                <td class="fit">No. of bank used <br>
+                    使用銀行的數量</td>
+                <td class="text-wrap"></td>
+            </tr>
+
+            <tr>
+                <td class="fit">No. of bank transactions <br> (monthly) <br>
+                    每月經銀行交易的數量</td>
+                <td class="text-wrap"></td>
+                <td class="fit">No. of non-bank transactions <br>(monthly) <br>
+                    每月非經銀行交易的數量</td>
+                <td class="text-wrap"></td>
+            </tr>
+
+            <tr>
+                <td class="fit">Any property? <br>
+                    有否物業?</td>
+                <td class="text-wrap">Yes /No</td>
+                <td class="fit">Any motor vehicle? <br>
+                    有否車輛?</td>
+                <td class="text-wrap">Yes /No</td>
+            </tr>
 
 
-    </table>
+            <tr>
+                <td class="fit">No. of loans / hire purchases <br>
+                    貸款/分期付款數目</td>
+                <td class="text-wrap"></td>
+                <td class="fit">No. of employees <br>
+                    員工人數</td>
+                <td class="text-wrap"></td>
+            </tr>
 
 
+        </table>
+
+    </div>
 
 
 
