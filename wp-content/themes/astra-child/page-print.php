9f5c8ee9d0f405101d4a45d3b29744cc80c13e1b
kaiyu li <krisfk@gmail.com>
2022-03-02 23:24:07 +0800

    .

--- wp-content/themes/astra-child/page-print.php
+++ wp-content/themes/astra-child/page-print.php
@@ -4,9 +4,15 @@ get_header();
 
 <div class="container mx-auto">
     <div class="print-header">
+
         <img class="ca-print-logo" src="<?php echo get_site_url();?>/wp-content/uploads/2022/03/ca-print-logo-1.jpg"
             alt="">
+
+        <div class="float-end">Trust or Company Service Provider Liscense No. <br>
+            信托或公司服務提供牌照:TC007204</div>
     </div>
+
+
 
 
 
