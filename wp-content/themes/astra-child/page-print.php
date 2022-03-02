a2a572223db3d25196e20c2e46dbe5be47f9b292
kaiyu li <krisfk@gmail.com>
2022-03-02 23:15:54 +0800

    .

--- wp-content/themes/astra-child/page-print.php
+++ wp-content/themes/astra-child/page-print.php
@@ -1,24 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+get_header(); 
+?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<div class="print-header">
 
-<body>
-
-    <div class="print-header">
-
-        <img class="ca-print-logo" src="<?php echo get_site_url();?>/wp-content/uploads/2022/03/ca-print-logo-1.jpg"
-            alt="">
-    </div>
+    <img class="ca-print-logo" src="<?php echo get_site_url();?>/wp-content/uploads/2022/03/ca-print-logo-1.jpg" alt="">
+</div>
 
 
 
-
+<?php get_footer(); ?>
 </body>
 
 </html>
