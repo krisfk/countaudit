6670a9607ba63bf42d671f7836b0f583c9bea311
kaiyu li <krisfk@gmail.com>
2022-03-03 23:05:09 +0800

    .

--- wp-content/themes/astra-child/page-print.php
+++ wp-content/themes/astra-child/page-print.php
@@ -509,7 +509,7 @@ get_header();
                 </tr>
                 <tr>
                     <td>
-                        <div class="mb-4">
+                        <div class="mb-2">
                             <p><b class="d-block text-center">Plan A (HK$1,400) </b></p>
                             <ul class="m-0 mt-2 p-0 ps-4">
                                 <li class="ls1">擔任法定公司秘書 Act as Company Secretary</li>
@@ -526,7 +526,7 @@ get_header();
                 </tr>
                 <tr>
                     <td>
-                        <div class="mb-4">
+                        <div class="mb-2">
                             <p><b class="d-block text-center">Plan B (HK$2,200) </b></p>
                             <ul class="m-0 mt-2 p-0 ps-4">
                                 <li class="ls1">擔任法定公司秘書 Act as Company Secretary</li>
@@ -553,7 +553,7 @@ get_header();
                 </tr>
                 <tr>
                     <td>
-                        <div class="mb-4">
+                        <div class="mb-2">
                             <p><b class=" d-block text-center">Plan C (HK$3,000) </b></p>
                             <ul class="m-0 mt-2 p-0 ps-4">
                                 <li class="ls1">擔任法定公司秘書 Act as Company Secretary</li>
