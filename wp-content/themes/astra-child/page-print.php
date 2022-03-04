636b8595441de74604dfa57ce6bf727d7a8b07b5
kaiyu li <krisfk@gmail.com>
2022-03-04 18:39:56 +0800

    .

--- wp-content/themes/astra-child/page-print.php
+++ wp-content/themes/astra-child/page-print.php
@@ -100,21 +100,21 @@ get_header();
                 <tr>
                     <td class="fit text-nowrap">Has performed audit before?<br>
                         是否曾進行核數</td>
-                    <td class="fit text-nowrap">Yes / No If Yes,<br>please provide the related year ended date<br>
+                    <td class="fit text-nowrap">Yes / No If Yes, please provide the related year ended date<br>
                         是 / 否 如是，請提供相關年結日</td>
                     <td class="text-wrap" colspan="2"></td>
                 </tr>
                 <tr>
                     <td class="fit">Has declared profits tax before?<br>
                         是否曾申報利得稅</td>
-                    <td class="fit">Yes / No If Yes,<br>please provide the related year of assessment<br>
+                    <td class="fit">Yes / No If Yes, please provide the related year of assessment<br>
                         是 / 否 如是，請提供相關課稅年度</td>
                     <td class="text-wrap" colspan="2"></td>
                 </tr>
                 <tr>
                     <td class="fit">Has done accounting before?<br>
                         是否曾做會計帳</td>
-                    <td class="fit">Yes / No If Yes,<br>please provide the related year ended date<br>
+                    <td class="fit">Yes / No If Yes, please provide the related year ended date<br>
                         是 / 否 如是，請提供相關年結日</td>
                     <td class="text-wrap" colspan="2"></td>
                 </tr>
