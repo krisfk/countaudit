5556f0722a58de763052dfe9d304f1ea939e9b52
kaiyu li <krisfk@gmail.com>
2022-03-06 04:20:34 +0800

    .

--- wp-content/themes/astra-child/page-print.php
+++ wp-content/themes/astra-child/page-print.php
@@ -774,7 +774,7 @@ get_header();
                 <tr>
                     <td>
                         <div class="text-start">
-                            <?php echo get_field('virtual_office');?>
+                            <?php print_r(get_field('virtual_office'));?>
                             ❑ 虛擬辦工室 Virtual Office
                         </div>
 
