bf3438a770b99cf4e8f6561eeffd042f25e6e9a6
kaiyu li <krisfk@gmail.com>
2022-03-07 05:59:59 +0800

    .

--- wp-content/themes/astra-child/header.php
+++ wp-content/themes/astra-child/header.php
@@ -51,6 +51,8 @@ if ( ! defined( 'ABSPATH' ) ) {
     <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
     <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css" />
     <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <script src='https://www.google.com/recaptcha/api.js'></script>
+
 </head>
 
 <body <?php astra_schema_body(); ?> <?php body_class(); ?>>
