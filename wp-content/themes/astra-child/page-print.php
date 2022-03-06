518599d3ef51333b43f97b6c86072fc6b3e75b3d
kaiyu li <krisfk@gmail.com>
2022-03-06 18:12:23 +0800

    .

--- wp-content/themes/astra-child/page-print.php
+++ wp-content/themes/astra-child/page-print.php
@@ -1735,7 +1735,7 @@ get_header();
             <?php 
         //    (11 - 4) /5
            $additional_page = $count_shareholders_and_directors > 4 ? ceil(($count_shareholders_and_directors - 4)/5) :0;
-        //    $additional_page=50;
+           $additional_page=10;
 
             // echo  $additional_page;
             for($i=0;$i<$additional_page;$i++)
