2de82d00d115fff012c2319ae83b9c8d55256ac2
kaiyu li <krisfk@gmail.com>
2022-03-07 04:48:21 +0800

    .

--- wp-content/themes/astra-child/page-print.php
+++ wp-content/themes/astra-child/page-print.php
@@ -3181,6 +3181,8 @@ get_header();
                 <div class="blank-area">- BLANK AREA -</div>
                 <div class="blank-area">- BLANK AREA -</div>
                 <div class="blank-area">- BLANK AREA -</div>
+                <div class="blank-area">- BLANK AREA -</div>
+                <div class="blank-area">- BLANK AREA -</div>
 
                 <table class="form-table mt-0 mb-0">
 
