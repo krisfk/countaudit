06a7bc142f91a0db07370d0c052e94dfc1f6b569
kaiyu li <krisfk@gmail.com>
2022-03-08 22:53:29 +0800

    .

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -135,6 +135,6 @@ add_filter( 'wp_mail_content_type','wpse27856_set_content_type' );
 
 function receive_email_func() {
 	global $receive_email;
-	$receive_email = 'krisfk@gmail.com';
+	$receive_email = 'info@countaudit.hk';
 }
 add_action( 'after_setup_theme', 'receive_email_func' );
