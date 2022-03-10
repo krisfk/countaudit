690875b94600b17068e3741d070ff8b8a7b338ed
kaiyu li <krisfk@gmail.com>
2022-03-10 16:41:59 +0800

    .

--- wp-content/themes/astra-child/page-test.php
+++ wp-content/themes/astra-child/page-test.php
@@ -12,7 +12,7 @@
     <?php 
     $to='krisfk@gmail.com';
     $subject='testing -subject';
-    $message='fdsfdffdsfs<img src="http://64.227.13.14/testwp/wp-content/uploads/2022/03/octobus.jpeg">';
+    $message='fdsfdffdsfs<img src="https://i.ibb.co/SV0h427/octobus.jpg">';
     wp_mail( $to, $subject, $message );
     
     ?>
